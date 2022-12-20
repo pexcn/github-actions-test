42ef7021c3e4d4e96adc010d21a0461e92246300
pexcn <i@pexcn.me>
2022-12-20 20:30:52 +0800

    test

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-apk update \
-  && apk add --no-cache --virtual .build-deps musl-dev git
-
-git clone https://github.com/shadowsocks/shadowsocks-rust.git
-cd shadowsocks-rust
-RUSTFLAGS="-C link-arg=-s" cargo build --release --features "local-redir"

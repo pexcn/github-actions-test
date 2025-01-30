b1e034b58459172e4954df28c4f86d4d3ecea513
Sing Yu Chan <pexcn97@gmail.com>
2025-01-30 22:23:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-git clone https://github.com/rssnsj/portfwd
-cd portfwd/src
-export CC="ccache gcc"
-make
-
-ls -al /builder/.cache/ccache
 echo
-echo
-echo
-find /builder/.cache/ccache

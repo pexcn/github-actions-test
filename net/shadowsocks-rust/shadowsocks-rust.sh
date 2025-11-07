a4d32b2a782237a8dff5738430bf4d1517b37732
pexcn <pexcn97@gmail.com>
2025-11-07 21:52:03 +0800

    up

--- net/shadowsocks-rust/shadowsocks-rust.sh
+++ net/shadowsocks-rust/shadowsocks-rust.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-if [ "$CHILLING_EFFECT" = 1 ]; then
-  exec $(echo "$@" | sed '
-    s/ssservice/service/;
-    s/ssserver/server/;
-    s/sslocal/local/;
-    s/ssmanager/manager/;
-    s/ssurl/url/;
-    s/obfs-server/mix-server/;
-    s/obfs-local/mix-local/;
-    s/v2ray-plugin/pv-plugin/;
-    s/xray-plugin/px-plugin/;
-    s/qtun-server/qt-server/;
-    s/qtun-client/qt-client/
-  ')
-else
-  exec "$@"
-fi
- 
+          changed_files='${{ steps.detect-changes.outputs.all_changed_files }}'
+          changed_files_json=$(echo "$changed_files" | jq -Rc 'split(" ")')
+          matrix_json=$(jq -c -n --argjson ctxs "$changed_files" '
+            {
+              include: [
+                $ctxs[] | . as $ctx | {
+                  BUILD_CONTEXT: $ctx,
+                  TARGET_PLATFORMS: (
+                    if (
+                      [
+                        "buildbot/bookworm",
+                        "buildbot/bookworm-android",
+                        "buildbot/bookworm-openwrt"
+                      ] | index($ctx)
+                    ) != null
+                    then
+                      "linux/amd64"
+                    else
+                      "linux/amd64,linux/arm64"
+                    end
+                  )
+                }
+              ]
+            }
+          ')
+          echo "matrix_json=$matrix_json" >> $GITHUB_OUTPUT
